8a7b54760d91c323c0818782dadbcab1cb416cc5
Ajani Douglas <douglasajani2@gmail.com>
2026-06-09 02:53:28 -0400

    Main Router file changed imports to debug netlify error

--- MainRouter.jsx
+++ MainRouter.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
-import About from './src/about'
-import Contact from './src/contact'
-import Education from './src/education'
-import Project from './src/project'
+import About from './about'
+import Contact from './contact'
+import Education from './education'
+import Project from './project'
 import Layout from './components/Layout'
-import Services from './src/Services'
+import Services from './services'
 const MainRouter = () => {
 return (<div>
 <Layout/>
